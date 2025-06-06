c274cc1548d35ffebb75208e412ef41c896a450d
Max6-spec <3324518920@qq.com>
2025-06-06 23:25:51 +0800

    Add files via upload

--- sanjianke.py
+++ sanjianke.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title='相册')
 
 images = [{
-    "ur1":"https://www.sea-help.eu/wp-content/uploads/2023-04-05_seahelp_meeresleuchten-bioluminescence.jpg",caption='海洋'},
-    {"ur2":"https://images6.alphacoders.com/773/773201.jpg",caption='海豚'},
-    {"ur3":"https://wallup.net/wp-content/uploads/2019/10/97666-landscape-sea-sunset-beauty-coast-beach-water-sky-lighthouse-sand-reflection-shine.jpg"caption='落日'}]
+    "ur1":"https://www.sea-help.eu/wp-content/uploads/2023-04-05_seahelp_meeresleuchten-bioluminescence.jpg","caption":'海洋'},
+    {"ur2":"https://images6.alphacoders.com/773/773201.jpg","caption":'海豚'},
+    {"ur3":"https://wallup.net/wp-content/uploads/2019/10/97666-landscape-sea-sunset-beauty-coast-beach-water-sky-lighthouse-sand-reflection-shine.jpg","caption":'落日'}]
 
 # 初始化当前图片索引
 if 'current_image_index' not in st.session_state:
