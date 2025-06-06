b2cadadabc98a45917b94a5a8b3dab198b959815
Max6-spec <3324518920@qq.com>
2025-06-06 23:20:33 +0800

    Add files via upload

--- sanjianke.py
+++ sanjianke.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title='bird')
-st.image("https://www.lilesnet.com/wow/birds/1-birds.jpg", caption='小鸟')
+st.set_page_config(page_title='相册')
 
-import streamlit as st
-
-# 图片URL列表
 images = [
     "https://www.sea-help.eu/wp-content/uploads/2023-04-05_seahelp_meeresleuchten-bioluminescence.jpg",caption='海洋',
     "https://images6.alphacoders.com/773/773201.jpg",caption='海豚',
@@ -19,11 +15,8 @@ st.subheader("相册")
 
 # 显示当前图片
 current_image = images[st.session_state.current_image_index]
-st.image(current_image)
+st.image(current_image["url"], caption=current_image["caption"])
 
-# 图片标题（可以根据需要自定义）
-image_titles = ["海洋", "海豚", "落日"]
-st.write(image_titles[st.session_state.current_image_index])
 
 # 创建两列布局放置按钮
 col1, col2 = st.columns(2)
@@ -35,6 +28,7 @@ with col1:
         # 如果已经是第一张，循环到最后一张
         if st.session_state.current_image_index < 0:
             st.session_state.current_image_index = len(images) - 1
+        st.rerun()
 
 with col2:
     # 下一张按钮
@@ -43,3 +37,4 @@ with col2:
         # 如果已经是最后一张，循环到第一张
         if st.session_state.current_image_index >= len(images):
             st.session_state.current_image_index = 0
+        st.rerun()
