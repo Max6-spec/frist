21fb44cda601a9b19c78baf2b91685dc33a161a6
Max6-spec <3324518920@qq.com>
2025-06-18 08:26:25 +0800

    Add files via upload

--- streamlit_predict.py
+++ streamlit_predict.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 )
 with st.sidebar:
-    st.image('images/rigth_logo.png',width=100)
+    st.image('rigth_logo.png',width=100)
     st.title('请选择页面')
     page=st.selectbox("请选择页面",['简介页面','预测分类页面'])
 if page=='简介页面':
@@ -72,7 +72,7 @@ elif page=="预测分类页面":
             if not submitted:
                 st.image(os.path.abspath('right_logo.png'),width=300)
             else:
-                st.image(f'{predict_result_species}.png',width=300)
+                st.image(f'predict_result_species.png',width=300)
         
 
 
